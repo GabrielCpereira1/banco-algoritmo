32f1ce6637f2726122d1fd7895ac943a851dd6d8
GabrielCpereira1 <gabrielcarlospereira20@gmail.com>
2026-04-16 19:59:02 -0300

    banko.py

--- banko.py
+++ banko.py
@@ -1,18 +1,26 @@
 usuario_correto = "Edson Dalbogas"
 senha_correta = "Edson123"
+
 saldo = 300.00
 limite = 5000.00
+
 def menu():
     while True:
-    print("\n--Univille Internet Banking--")
-    print("1. Acessar conta")
-    print("2. Encerrar sessão")
-    opcao = input("Digite a opção desejada:")
-    if opcao == "1":
-        if login():
-            menu_principal()
-    elif opcao == "2":
-        print("Sessão Encerrada")
-        break
+     print("\n- Univille Internet Banking -")
+     print("1. Acessar conta")
+     print("2. Encerrar sessão")
+     opcao = input("Escolha sua opção: ")
+
+     if opcao == "1":
+         if login():
+             menu_principal()
+     elif opcao == "2":
+         print("Sessão Encerrada.")
+         break
+     else:
+         print("Opção inválida.")
+
+
+       
     
             
