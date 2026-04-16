f94c07dd3b2551739e2c09934bed58a9231e5702
GabrielCpereira1 <gabrielcarlospereira20@gmail.com>
2026-04-16 18:51:47 -0300

    banko.py

--- banko.py
+++ banko.py
@@ -3,4 +3,16 @@ senha_correta = "Edson123"
 saldo = 300.00
 limite = 5000.00
 def menu():
-    print
+    while True:
+    print("\n--Univille Internet Banking--")
+    print("1. Acessar conta")
+    print("2. Encerrar sessão")
+    opcao = input("Digite a opção desejada:")
+    if opcao == "1":
+        if login():
+            menu_principal()
+    elif opcao == "2":
+        print("Sessão Encerrada")
+        break
+    
+            
