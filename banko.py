84fdc3192e18ccf1979d58b6ac6c38e83417f0fa
GabrielCpereira1 <gabrielcarlospereira20@gmail.com>
2026-04-18 11:44:55 -0300

    login e menu principal

--- banko.py
+++ banko.py
@@ -20,6 +20,37 @@ def menu():
      else:
          print("Opção inválida.")
 
+def login():
+    tentativas = 3
+    while tentativas > 0:
+        usuario = input("Usuário: ")
+        senha = input("Senha: ")
+
+        if usuario == usuario_correto and senha == senha_correta:
+            print("Login realizado com sucesso")
+            return True
+        else:
+            tentativas -= 1
+            if tentativas == 0:
+                print(f"Usuário ou senha incorretos. Tentativas restantes: {tentativas}")
+            else:
+                print("Sem mais tentativas.")
+                print("Sessão Encerrada.")
+                exit()
+
+def menu_principal():
+    global saldo
+
+    while True:
+        print("\n--Univille Internet Banking--")
+        print("1. Consultar Saldo")
+        print("2. Realizar Saque")
+        print("3. Realizar Depósito")
+        print("4. Consultar Limite")
+        print("5. Encerrar Sessão")    
+        opcao = input("Escolha sua opção:")
+
+        
 
        
     
