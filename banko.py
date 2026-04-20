27edd77490d90527b47d0ebdc54174688c7b57a3
GabrielCpereira1 <gabrielcarlospereira20@gmail.com>
2026-04-20 10:47:28 -0300

    programa completo

--- banko.py
+++ banko.py
@@ -49,8 +49,58 @@ def menu_principal():
         print("4. Consultar Limite")
         print("5. Encerrar Sessão")    
         opcao = input("Escolha sua opção:")
+        if opcao == "1":
+            consultar_saldo()
+        elif opcao == "2":
+            realizar_saque()
+        elif opcao == "3":
+            realizar_deposito()
+        elif opcao == "4":
+            consultar_limite()
+        elif opcao == "5":
+            print("Sessão Encerrada.")
+            break
+        else:
+            print("Opção inválida.")
 
+def consultar_saldo():
+    print(f"Saldo R$: {saldo:.2f}")
+
+def realizar_saque():
+    global saldo
+    try:
+        valor = float(input("Valor do saque: R$ "))
         
+        if valor <= 0:
+            print("Valor inválido.")
+        elif valor > (saldo + limite):
+            print("Saldo insuficiente.")
+        else:
+            saldo -= valor
+            print("Saque realizado")
+    except ValueError:
+        print("Valor inválido.")
+
+def realizar_deposito():
+    global saldo
+    try:
+        valor = float(input("Valor do depósito: R$ "))
+        
+        if valor <= 0:
+            print("Valor inválido.")
+        else:
+            saldo += valor
+            print("Depósito realizado")
+    except ValueError:
+        print("Valor inválido.")
+
+def consultar_limite():
+    print(f"Limite da Conta R$: {limite:.2f}")
+
+# Executar o sistema
+menu()
+
+       
 
        
     
