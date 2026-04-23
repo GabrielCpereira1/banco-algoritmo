018a737a81baae6eda6f71d51d9c5b66e719843e
Duducacle <duducacle2005@gmail.com>
2026-04-23 09:41:14 -0300

    banko.py

--- banko.py
+++ banko.py
@@ -3,22 +3,25 @@ senha_correta = "Edson123"
 
 saldo = 500.00
 limite = 3000.00
+carteira = 1000.00
+
+
 
 def menu():
     while True:
-     print("\n- Univille Internet Banking -")
-     print("1. Acessar conta")
-     print("2. Encerrar sessão")
-     opcao = input("Escolha sua opção: ")
+        print("\n- Univille Internet Banking -")
+        print("1. Acessar conta")
+        print("2. Encerrar sessão")
+        opcao = input("Escolha sua opção: ")
 
-     if opcao == "1":
-         if login():
-             menu_principal()
-     elif opcao == "2":
-         print("Sessão Encerrada.")
-         break
-     else:
-         print("Opção inválida.")
+        if opcao == "1":
+            if login():
+                menu_principal()
+        elif opcao == "2":
+            print("Sessão Encerrada.")
+            break
+        else:
+            print("Opção inválida.")
 
 def login():
     tentativas = 3
@@ -36,17 +39,21 @@ def login():
     print("Sessão Encerrada.")
     return False
 
+
+
 def menu_principal():
     global saldo
 
     while True:
-        print("\n--Univille Internet Banking--")
+        print("\n-- Univille Internet Banking --")
         print("1. Consultar Saldo")
         print("2. Realizar Saque")
         print("3. Realizar Depósito")
         print("4. Consultar Limite")
-        print("5. Encerrar Sessão")    
-        opcao = input("Escolha sua opção:")
+        print("5. Consultar Dinheiro Físico")
+        print("6. Encerrar Sessão")
+        opcao = input("Escolha sua opção: ")
+
         if opcao == "1":
             consultar_saldo()
         elif opcao == "2":
@@ -56,44 +63,60 @@ def menu_principal():
         elif opcao == "4":
             consultar_limite()
         elif opcao == "5":
+            consultar_carteira()
+        elif opcao == "6":
             print("Sessão Encerrada.")
             break
         else:
             print("Opção inválida.")
 
+
 def consultar_saldo():
     print(f"Saldo R$: {saldo:.2f}")
 
+
 def realizar_saque():
-    global saldo
+    global saldo, carteira
     try:
         valor = float(input("Valor do saque: R$ "))
-        
+
         if valor <= 0:
             print("Valor inválido.")
         elif valor > (saldo + limite):
             print("Saldo insuficiente/Limite Atingido.")
         else:
             saldo -= valor
+            carteira += valor
             print("Saque realizado")
     except ValueError:
         print("Valor inválido.")
 
+
 def realizar_deposito():
-    global saldo
+    global saldo, carteira
     try:
         valor = float(input("Valor do depósito: R$ "))
-        
+
         if valor <= 0:
             print("Valor inválido.")
+        elif valor > carteira:
+            print("Você não tem esse valor em dinheiro físico.")
         else:
             saldo += valor
+            carteira -= valor
             print("Depósito realizado")
     except ValueError:
         print("Valor inválido.")
 
+
 def consultar_limite():
     print(f"Limite da Conta R$: {limite:.2f}")
+
+
+def consultar_carteira():
+    print(f"Dinheiro em carteira: R$ {carteira:.2f}")
+
+
 
 # Executar o sistema
 menu()
